1f3098ffbfbf48a8c47a9a43641122c4e82c64d5
Beef Webster <c_goldsmith@yahoo.com>
2022-09-14 19:10:40 -0700

    small changes

--- assets/script.js
+++ assets/script.js
@@ -30,6 +30,20 @@ fetch('https://yelp-com.p.rapidapi.com/search/nearby/37.788719679657554/-122.400
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
+// yelp business details	
+const options3 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '48c1efcdf0mshbc4f579e09271eep1736d1jsn77c5309cc348',
+		'X-RapidAPI-Host': 'yelp-com.p.rapidapi.com'
+	}
+};
+	
+	fetch('https://yelp-com.p.rapidapi.com/business/DAiqwrmv19Uv-I1bOoAJCQ', options3)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+
 
 // Google Maps API
 const options2 = {
